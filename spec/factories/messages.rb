f1aa245ec7f6e4e5e9347532ac00066196d0ae35
kaclar-ef <62864518+kaclar-ef@users.noreply.github.com>
2020-04-10 17:44:17 +0900

    create test method by rspec

---
+++ spec/factories/messages.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :message do
+    content {Faker::Lorem.sentence}
+    image {File.open("#{Rails.root}/public/images/test_image.jpg")}
+    group
+    user
+  end
+end
