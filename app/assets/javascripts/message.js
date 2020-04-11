f442c471c0d1a29b06b8f4cee51338437b7e508d
kaclar-ef <62864518+kaclar-ef@users.noreply.github.com>
2020-04-11 12:02:00 +0900

    Update message.js -- create 'done function' and 'fail function'

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function() {
+
+  function buildHTML(message){
+    if (message.image) {
+      let html = `<div class="chat-items">
+                    <div class="chat-items__top">
+                      <div class="chat-items__top--black">
+                        ${message.user_name}
+                      </div>
+                      <div class="chat-items__top--gray">
+                        ${message.created_at} 
+                      </div>
+                    </div>
+                    <div class="chat-items__bottom">
+                       <div class="chat-items__bottom__text">
+                         ${message.content}
+                       </div>
+                       <img src= ${message.image}>
+                    </div>
+                  </div>`
+      return html
+    } else {
+      let html = `<div class="chat-items">
+                    <div class="chat-items__top">
+                      <div class="chat-items__top--black">
+                        ${message.user_name}
+                      </div>
+                      <div class="chat-items__top--gray">
+                        ${message.created_at} 
+                      </div>
+                    </div>
+                    <div class="chat-items__bottom">
+                      <div class="chat-items__bottom__text">
+                        ${message.content}
+                      </div>
+                    </div>
+                  </div>`
+      return html
+      }
+  }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     let formData = new FormData(this);
@@ -11,5 +51,15 @@ $(function() {
       processData: false,
       contentType: false
     })
+    .done(function(message) {
+      let html = buildHTML(message);
+      $('.chat-main__message-list').append(html);
+      $('form')[0].reset();
+      $('.form-input__btn').prop('disabled', false);
+      $('.chat-main__message-list').animate({scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   });
 })
